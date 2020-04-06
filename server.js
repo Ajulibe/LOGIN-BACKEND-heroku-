dcf62a491217157d54c91432efd67a05bf6ccd99
akachukwu <akachi_benji@yahoo.com>
2020-04-06 23:07:47 +0100

    added get

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("done");
+});
+
 //posting for signin
 app.post("/signin", (req, res) => {
   const { email, password } = req.body;
